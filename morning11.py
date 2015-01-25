bc882a5006ca54d3ff8fd2c2e080a47411310d1e
Fabien Benetou <fabien-services@benetou.fr>
2015-01-25 19:00:24 +0100

    Update with proper URL with new domain name

--- morning11.py
+++ morning11.py
@@ -28,6 +28,8 @@ import simplejson
 import cStringIO
 import os
 import os.path
+import hashlib
+import pickle
 
 def loadData(resultsfile):
     jsonresults = open(resultsfile)
@@ -48,6 +50,11 @@ class NewCreation:
     blendingmethod = "" # currently only one, could also be a class
     generationtime = "" #DateTime.now() 
      
+class SrcImage:
+    def __init__(self, path, keyword ):
+        self.path=path
+        self.keyword=keyword
+
 def blend(imagestouse, ad_keywords):
     
     os.system("montage " + ' '.join(str(x) for x in imagestouse) + " result_" + '_'.join(str(x) for x in ad_keywords) + ".png")
@@ -62,6 +69,12 @@ def addImageToLibrary(keyword, startIndex):
     f = fetcher.open(searchUrl)
     deserialized_output = simplejson.load(f)
     imageUrl = deserialized_output['responseData']['results'][0]['unescapedUrl']
+    
+    hash_object = hashlib.md5(imageUrl.encode())
+    imageHash = hash_object.hexdigest()
+    newimagepath = srcimagespath + imageHash + ".png"
+    if os.path.isfile(newimagepath):
+        return 1
     # limited to only 4 results
     try:
         #should check if already downloaded 
@@ -72,18 +85,32 @@ def addImageToLibrary(keyword, startIndex):
         return 1 
     img.quality(100) #full compression
     img.magick('PNG')
-    newimagepath = "srcimages/img" + keyword + ".png"
     #consider using hashing or URL instead
     img.write(newimagepath)
-    return newimagepath
+    newimage = SrcImage(newimagepath,keyword)
+    return newimage
 
 if __name__ == "__main__":
-    
+
+    rooturl = "done.with.software"
+    #important for QRcode 
+    srcimagespath = "srcimages/img" 
     resultsfile = "results.json"
     imagestouse = []
-    ad_format = 'image'
+    imagesdb = []
     ad_keywords = ['banana','car','Blue']
     
+
+    previousdata = "data.pkl"
+    if os.path.isfile(previousdata):
+        with open(previousdata, 'rb') as input:
+            imagesdb = pickle.load(input)
+
+    for i in imagesdb:
+        print i.keyword
+        print i.path
+
+    exit()
     #expected_number_results = 10
     #result_size = 1000
     
@@ -97,8 +124,14 @@ if __name__ == "__main__":
         while newimage == 1:
             i += 1
             newimage = addImageToLibrary(keyword, i)
-        imagestouse.append(newimage)
+        imagestouse.append(newimage.path)
+        imagesdb.append(newimage)
     
+    with open(previousdata, 'wb') as output:
+        pickle.dump(imagesdb, output, pickle.HIGHEST_PROTOCOL)
+    exit()
+    
+    # should JSON dump else either re-downloading every time or losing data like keyword used
     #print "manual test : https://www.google.com/searchbyimage?&image_url="+serverRootUrl+resultname
     # print "Not h-creative, make other ..."
     blend(imagestouse,ad_keywords)
